3d9aefac338cfae64589367e7380a1ad9de3bb20
Louise Monte <louisemonte@Louises-MacBook-Air.local>
2025-02-13 19:45:28 +0100

    laatste test

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require ('express')
 const app = express ()
 
-app.set('views engine', 'ejs'); // voegt toe dat we met ejs werken 
+app.set('view engine', 'ejs');
+ // voegt toe dat we met ejs werken 
 
 app 
 .use('/static', express.static('static'));
 
 app 
 .get('/', onhome)
+.get ('/contact', contact)
 .get ('/about', about)
+.get ('/login', login)
 .get ('/detail/:username', username)
 .listen (8000)
 
+function login(req, res){
+    res.send ('<h1> Hello bij login </h1>');
+}
+function contact(req, res){
+    res.send ('<h1> Hello bij contact </h1>');
+}
 
 function onhome( req, res){
     res.send ('<h1> Hello world </h1>');
@@ -24,3 +33,7 @@ function about( req, res){
 function username (req, res){
     res.render('detail', {username: req.params.username}); // req.params
 }
+
+app.use(function(req, res){
+    res.status(404).send("Not found prutsern");
+});
