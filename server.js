c22f1641f0b8f781ce2d5d2e42b4e06d169ec8cd
Louise Monte <louisemonte@Louises-MacBook-Air.local>
2025-02-13 17:02:44 +0100

    test met ejs

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require ('express')
 const app = express ()
 
-app 
-.use('/static', express.static('static'))
+app.set('views engine', 'ejs'); // voegt toe dat we met ejs werken 
 
+app 
+.use('/static', express.static('static'));
 
 app 
 .get('/', onhome)
-.get ("/about", about)
+.get ('/about', about)
+.get ('/detail/:username', username)
 .listen (8000)
+
 
 function onhome( req, res){
     res.send ('<h1> Hello world </h1>');
@@ -18,5 +21,6 @@ function about( req, res){
     res.send ('<h1> about </h1>');
 }
 
-
-
+function username (req, res){
+    res.render('detail', {username: req.params.username}); // req.params
+}
